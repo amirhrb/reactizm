bfc161f32c3fd7d4321703d9ec4faadf3294b87f
Amirhosein Rabibeigi <81120130+amirhrb@users.noreply.github.com>
2023-12-13 23:18:15 +0330

    Update index.js

--- pages/dall-e/index.js
+++ pages/dall-e/index.js
@@ -45,7 +45,7 @@ export default function index() {
           />
         </Head>
         <iframe
-          src="https://sleepy-einstein-dg9urq1wb.liara.run/dall-e"
+          src="https://gpt-no-auth.vercel.app/dall-e"
           style={{
             border: "none",
             width: "100%",
